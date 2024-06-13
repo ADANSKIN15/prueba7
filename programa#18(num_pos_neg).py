13f7cd9af18f3872b41408d9db8f428720914ca4
ADANSKIN15 <adansking22@gmail.com>
2024-06-13 11:34:25 -0500

    Add files via upload
    programa que verifica si es un numero positivo o negativo

---
+++ programa#18(num_pos_neg).py
@@ -0,0 +1,13 @@
+# Escribe un programa que solicite al usuario un número y determine si es positivo, 
+# negativo o cero
+
+numero = float(input('ingrese el valor: \n'))
+
+if numero >0:
+    print('el numero es positivo')
+    
+else:
+    print('el numero es negativo')
+print()   
+  
+print('FIN')
